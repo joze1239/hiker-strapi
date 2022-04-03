4ff3b697550136ed5f35b56bfa71672a533d339e
Joze Fartek <joze.fartek@gmail.com>
2022-04-03 20:12:14 +0200

    Update mountain relation

---
+++ src/api/mountain/controllers/mountain.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  mountain controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::mountain.mountain');
---
+++ src/api/mountain/routes/mountain.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * mountain router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::mountain.mountain');
