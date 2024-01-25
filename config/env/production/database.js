79f812678320d6e42a4a4a95b05a402098ce9c89
Joze Fartek <joze.fartek@gmail.com>
2024-01-25 21:16:00 +0100

    Debug database connection

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,22 +1,23 @@
-const parse = require('pg-connection-string').parse;
+// const parse = require("pg-connection-string").parse;
 
-console.log('DATABASE_URL', process.env )
-const config = parse(process.env.DATABASE_URL);
 
-module.exports = ({env}) => {
-  console.log('database.js', env)
- return {
-  connection: {
-    client: "postgres",
-    connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: false,
-    },
-    debug: false,
-  },
-}
+module.exports = ({ env }) => {
+  console.log("database.js", env);
+  return {
+
+  }
+  //  return {
+  //   connection: {
+  //     client: "postgres",
+  //     connection: {
+  //       host: config.host,
+  //       port: config.port,
+  //       database: config.database,
+  //       user: config.user,
+  //       password: config.password,
+  //       ssl: false,
+  //     },
+  //     debug: false,
+  //   },
+  // }
 };
