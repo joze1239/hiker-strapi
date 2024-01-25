32e2eea5014f3f1805b921e0eb5999b77398aaad
Joze Fartek <joze.fartek@gmail.com>
2024-01-25 21:42:16 +0100

    Update database config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,10 +1,7 @@
 const parse = require('pg-connection-string').parse;
 
 module.exports = ({ env }) => {
-  const databaseUrl = env('DATABASE_URL', '');
-console.log('databaseUrl', {databaseUrl})
-  const config = parse(databaseUrl);
-  console.log('config', {config})
+  const config = parse(env('DATABASE_URL', ''));
 
   return {
     connection: {
